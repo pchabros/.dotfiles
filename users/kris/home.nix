aa51b6e4740c962c25b1f8afd4835525283f8527
pchabros <pawel.chabros@yahoo.pl>
2023-06-09 20:32:27 +0200

    Zsh config moved into module

--- users/kris/home.nix
+++ users/kris/home.nix
@@ -15,47 +15,6 @@
       userName = "pchabros";
       userEmail = "pawel.chabros@yahoo.pl";
     };
-    zsh = {
-      enable = true;
-      initExtra = builtins.readFile ../../config/zsh/.zshrc;
-      dotDir = ".config/zsh";
-      enableAutosuggestions = true;
-      enableCompletion = true;
-      enableSyntaxHighlighting = true;
-      autocd = true;
-      sessionVariables = {
-        EDITOR = "nvim";
-        VISUAL = "nvim";
-        TERM = "xterm-256color";
-        FZF_DEFAULT_COMMAND = "rg --files --hidden --glob '!.git'";
-        FZF_CTRL_T_COMMAND = "$FZF_DEFAULT_COMMAND";
-        ZSH_TMUX_AUTOSTART = true;
-        ZSH_TMUX_CONFIG = "$HOME/.config/tmux/tmux.conf";
-      };
-      history = {
-        ignoreDups = true;
-        save = 1000000;
-        size = 1000000;
-      };
-      prezto = {
-        enable = true;
-        editor = {
-          keymap = "vi";
-          promptContext = true;
-          dotExpansion = true;
-        };
-        prompt.theme = "pure";
-      };
-      zplug = {
-        enable = true;
-        plugins = [
-          { name = "plugins/git"; tags = [ from:oh-my-zsh ]; }
-          { name = "plugins/completion"; tags = [ from:oh-my-zsh ]; }
-          { name = "plugins/tmux"; tags = [ from:oh-my-zsh ]; }
-          { name = "stedolan/jq"; }
-        ];
-      };
-    };
     rofi = {
       enable = true;
     };
@@ -96,7 +55,6 @@
     #avizo.enable = true;
   };
   xdg.configFile = {
-    "zsh/aliases".text = builtins.readFile ../../config/zsh/aliases;
     "tmux/tmux.conf".text = builtins.readFile ../../config/tmux/tmux.conf;
     "i3/config".text = builtins.readFile ../../config/i3/config;
     rofi = {
