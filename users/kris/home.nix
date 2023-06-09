31f86cdac685d94e2ee22bf31eaa552fc2f2daa1
pchabros <pawel.chabros@yahoo.pl>
2023-06-09 20:44:44 +0200

    Polybar removed

--- users/kris/home.nix
+++ users/kris/home.nix
@@ -21,26 +21,11 @@
       nix-direnv.enable = true;
     };
   };
-  services = {
-    polybar = {
-      enable = true;
-      script = ''
-        PATH=$PATH:${pkgs.i3}/bin polybar example &
-      '';
-      package = pkgs.polybar.override {
-        alsaSupport = true;
-        pulseSupport = true;
-        i3GapsSupport = true;
-      };
-    };
-    #avizo.enable = true;
-  };
   xdg.configFile = {
     "i3/config".text = builtins.readFile ../../config/i3/config;
     rofi = {
       source = ../../config/rofi;
       recursive = true;
     };
-    "polybar/config.ini".text = builtins.readFile ../../config/polybar/config.ini;
   };
 }
