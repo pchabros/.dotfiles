c5d46c0a680a8aad46dac1e141ba4ec2ce5d3d56
pchabros <pawel.chabros@yahoo.pl>
2023-10-19 22:20:24 +0200

    Some additional (maybe redundant) fzf options

--- users/pawel_chabros/fzf/default.nix
+++ users/pawel_chabros/fzf/default.nix
@@ -3,6 +3,8 @@
     fzf = {
       enable = true;
       enableZshIntegration = true;
+      fileWidgetCommand = "fd --type f";
+      fileWidgetOptions = [ "--preview 'head {}'" ];
     };
   };
 }
