e7bffdbaec82f534f7ee4d0e5e10a95753a9be80
pchabros <pawel.chabros@yahoo.pl>
2024-01-05 09:10:40 +0100

    Angular LS added

--- users/pawel_chabros/neovim/config/plugins/nvim-lspconfig.lua
+++ users/pawel_chabros/neovim/config/plugins/nvim-lspconfig.lua
@@ -49,6 +49,7 @@ require("lspconfig").html.setup({
 })
 require("lspconfig").emmet_ls.setup({})
 require("lspconfig").tsserver.setup({})
+require("lspconfig").angularls.setup({})
 require("lspconfig").rnix.setup({})
 
 -- diagnostic icons
