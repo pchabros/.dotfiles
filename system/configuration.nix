47e21d49e72acd07d995f7d82eb6e0dd2de3f8c9
pchabros <pawel.chabros@yahoo.pl>
2024-09-23 08:19:35 +0200

    Firefox as default browser

--- system/configuration.nix
+++ system/configuration.nix
@@ -186,9 +186,22 @@
     '';
   };
 
-  xdg.portal = {
-    enable = true;
-    extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
+  xdg = {
+    portal = {
+      enable = true;
+      extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
+    };
+    mime = {
+      enable = true;
+      defaultApplications = {
+        "default-web-browser" = [ "firefox.desktop" ];
+        "text/html" = [ "firefox.desktop" ];
+        "x-scheme-handler/http" = [ "firefox.desktop" ];
+        "x-scheme-handler/https" = [ "firefox.desktop" ];
+        "x-scheme-handler/about" = [ "firefox.desktop" ];
+        "x-scheme-handler/unknown" = [ "firefox.desktop" ];
+      };
+    };
   };
 
   nixpkgs.config.allowUnfree = true;
