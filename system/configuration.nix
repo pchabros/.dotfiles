6c416deb3850706223908e0d4ee7ecb622654eaf
pchabros <pawel.chabros@yahoo.pl>
2024-10-16 07:50:07 +0200

    Firefox DEV as default browser

--- system/configuration.nix
+++ system/configuration.nix
@@ -203,12 +203,12 @@
     mime = {
       enable = true;
       defaultApplications = {
-        "default-web-browser" = [ "qutebrowser.desktop" ];
-        "text/html" = [ "qutebrowser.desktop" ];
-        "x-scheme-handler/http" = [ "qutebrowser.desktop" ];
-        "x-scheme-handler/https" = [ "qutebrowser.desktop" ];
-        "x-scheme-handler/about" = [ "qutebrowser.desktop" ];
-        "x-scheme-handler/unknown" = [ "qutebrowser.desktop" ];
+        "default-web-browser" = [ "firefox-devedition.desktop" ];
+        "text/html" = [ "firefox-devedition.desktop" ];
+        "x-scheme-handler/http" = [ "firefox-devedition.desktop" ];
+        "x-scheme-handler/https" = [ "firefox-devedition.desktop" ];
+        "x-scheme-handler/about" = [ "firefox-devedition.desktop" ];
+        "x-scheme-handler/unknown" = [ "firefox-devedition.desktop" ];
       };
     };
   };
