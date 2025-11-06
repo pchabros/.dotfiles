2db764c3a69b422ab296c646d6f91764b3d4087e
pchabros <pawel.chabros@yahoo.pl>
2025-11-06 09:01:00 +0100

    ClamAV added

--- system/nixos/configuration.nix
+++ system/nixos/configuration.nix
@@ -1,7 +1,9 @@
 {pkgs, ...}: {
   users.users.pawel_chabros = {
     packages = with pkgs; [
+      clamav
       keepass
+      libreoffice
       teams-for-linux
       thunderbird
       websocat
@@ -9,6 +11,11 @@
   };
   services = {
     blueman.enable = true;
+    clamav = {
+      daemon.enable = true;
+      scanner.enable = true;
+      updater.enable = true;
+    };
   };
   networking.wg-quick.interfaces = {
     qrk1.configFile = "/home/pawel_chabros/.config/vscode/pchabros1.conf";
