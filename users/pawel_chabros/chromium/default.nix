a9a58000dc5ae9dc8fbe52bf5b3e3adf47a4867d
pchabros <pawel.chabros@yahoo.pl>
2026-08-13 11:07:21 +0200

    Chromium default browser

--- users/pawel_chabros/chromium/default.nix
+++ users/pawel_chabros/chromium/default.nix
@@ -1,4 +1,15 @@
 {
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "x-scheme-handler/http" = "chromium.desktop";
+      "x-scheme-handler/https" = "chromium.desktop";
+      "text/html" = "chromium.desktop";
+      "x-scheme-handler/about" = "chromium.desktop";
+      "x-scheme-handler/unknown" = "chromium.desktop";
+    };
+  };
+
   programs.chromium = {
     enable = true;
     extensions = [
